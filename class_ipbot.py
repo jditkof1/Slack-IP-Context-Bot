4807a0b6baf46825d1aa367abbd3d394dcf2d17a
Jacob Ditkoff <jditkof1@binghamton.edu>
2021-10-31 16:54:00 -0400

    Update class_ipbot.py
    Changes:
    - Fixing the formatting of the final message the bot will send to the channel.
    - Catching when an IP address is invalid or is not in VirusTotal's v2 database
    - Editing variable names for clarity

--- class_ipbot.py
+++ class_ipbot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 
 
 class IPBot:
@@ -7,9 +8,7 @@ class IPBot:
         "type": "section",
         "text": {
             "type": "mrkdwn",
-            "text": (
-                "Info on your posted IP Address... \n\n"
-            ),
+            "text": "Info on your posted IP Address... \n\n"
         },
     }
 
@@ -17,21 +16,34 @@ class IPBot:
         self.channel = channel
 
     def _get_IP_info(self, ip_address):
-
         api_key = os.environ.get("APIKEY")
-        url = 'https://virustotal.com/vtapi/v2/url/report'
-        params = {'apikey': api_key,        # Virustotal API key exported to OS for security reasons
+        virustotal_url = 'https://virustotal.com/vtapi/v2/url/report'
+        params = {'apikey': api_key,  # Virustotal API key exported to OS for security reasons
                   'resource': 'https://' + ip_address}
-        r = requests.get(url, params=params)
+        r = requests.get(virustotal_url, params=params)
         initial_response = r.json()
-        formatted_response = f"""
-            IP Address: {initial_response['url']}
-            Date IP address was last scanned: {initial_response['scan_date']}
-            Total number of scans: {initial_response['total']}
-            Positive detections: {initial_response['positives']}
-            Individual scans: {initial_response['scans']}
-            """
-        return formatted_response
+
+        formatted_string = "No response from VirusTotal.\n"
+
+        if initial_response['response_code'] == 0:
+            formatted_string = ip_address + " is not in VirusTotal's IP database."
+
+        if initial_response['response_code'] == 1:
+            formatted_string = f"""
+                IP Address: {initial_response['url']}
+                Date IP address was last scanned: {initial_response['scan_date']}
+                Total number of scans: {initial_response['total']}
+                Positive detections: {initial_response['positives']}
+                """
+
+        formatted_block = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": formatted_string
+            },
+        }
+        return formatted_block
 
     def create_message(self, ip_address):
         return \
@@ -40,7 +52,6 @@ class IPBot:
                 "blocks":
                     [
                         self.IP_BLOCK,
-                        *self._get_IP_info(ip_address),
+                        self._get_IP_info(ip_address),
                     ],
             }
-
